44c505d8c9446a8b9432ee03e3dd9d122bea1ecd
Diwakar <30636732+divi2698@users.noreply.github.com>
2022-08-18 23:35:31 +0530

    - add application properties file for both tests and source - reformat code to 4 spaces

--- src/main/java/com/encrypter/enc/backend/EncBackendApplication.java
+++ src/main/java/com/encrypter/enc/backend/EncBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EncBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EncBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EncBackendApplication.class, args);
+    }
 
 }
