9cd759b6ed7ba634ce4f6f5b142d1bd7b97b5df2
Diwakar <30636732+divi2698@users.noreply.github.com>
2022-08-18 23:17:35 +0530

    project setup

---
+++ src/main/java/com/encrypter/enc/backend/EncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.encrypter.enc.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encrypter/enc/backend/EncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encrypter.enc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
